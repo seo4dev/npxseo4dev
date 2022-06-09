372089c09facbeadcf5f0715d1f5c89b083dbdd3
seo4dev <seofordev@gmail.com>
2022-06-09 13:44:27 +0100

    published on npm

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hi friend! I'm Ash Elmer
+
+## A 👨‍💻 frontend web developer with ✏️ graphic design background, 🚀  SEO enthusiast, addicted to 📚 learning and excited to share everything I learn with you
+
+This is my Links card package on npmjs, to see the links just open your terminal and type: npx seo4dev.
+you'll find my [website link](hhhtps://seo4dev.com) and all my social media links as well.
+
+Thanks for your interest and see you soon with an other interesting package.
--- scripts/links.mjs
+++ scripts/links.mjs
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
 import boxen from "boxen";
 import chalk from "chalk";
+import asciify from "asciify-image";
+
 
 const boxenOptions = {
   padding: 1,
@@ -7,36 +11,56 @@ const boxenOptions = {
   title: "Seo4dev",
   titleAlignment: "center",
   borderStyle: "double",
-  borderColor: "#ffb626",
+  borderColor: "#DC143C",
 };
 
 const intro = chalk.bold(
-  "Hi friend! I'm Ash. frontend web developer with graphic design background, SEO enthusiast, addicted to learning and excited to share everything I learn with you.\n\nFind me on the internet.\n\n"
+  "Hi friend! I'm Ash. frontend web developer with graphic design background, SEO enthusiast, addicted to learning and excited to share everything I learn with you.\n\nYou can find me on following links:\n\n"
 );
 
 const links = [
   {
-    name: chalk.hex("#fff").bgHex("#0f111a")("Website"),
+    name: chalk.hex("#fff").bgHex("#DC143C")("Website"),
     url: "   https://seo4dev.com",
   },
   {
-    name: chalk.hex("#fff").bgHex("#000")("DEV.to"),
-    url: "    https://dev.to/seofordev",
+    name: chalk.hex("#fff").bgHex("#1DA1F2")("Twitter"),
+    url: "   https://twitter.com/seofordev",
   },
   {
     name: chalk.hex("#fff").bgHex("#6e5494")("GitHub"),
     url: "    https://github.com/seo4dev",
   },
   {
-    name: chalk.hex("#fff").bgHex("#582be8")("Polywork"),
-    url: "  https://polywork.com/seo4dev",
+    name: chalk.hex("#fff").bgHex("#4267B2")("Facebook"),
+    url: "   https://www.facebook.com/Seo4Dev/",
   },
   {
-    name: chalk.hex("#fff").bgHex("#1DA1F2")("Twitter"),
-    url: "   https://twitter.com/seofordev",
+    name: chalk.hex("#fff").bgHex("#000")("DEV.to"),
+    url: "    https://dev.to/seofordev",
+  },
+  {
+    name: chalk.hex("#fff").bgHex("#582be8")("Polywork"),
+    url: "  https://polywork.com/seo4dev",
   },
 ];
 
 const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
 
 console.log(boxen(intro + linkList, boxenOptions));
+
+var options = {
+  fit: "box",
+  width: 25,
+  height: 25,
+};
+
+asciify("./seo4dev.png", options)
+  .then(function (asciified) {
+    // Print asciified image to console
+    console.log(asciified);
+  })
+  .catch(function (err) {
+    // Print error to console
+    console.error(err);
+  });
